9a0cb47aa95cda61d7a63976eba93953845475e7
babaygo <simon.laurent@epsi.fr>
2026-05-14 21:40:54 +0200

    add tailwindcss/vite

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import "./App.css";
-import { socket } from "./services/socket";
+import { socket } from "./types/socket";
 
 function App() {
   useEffect(() => {
     socket.emit("join_room", "test-room");
   }, []);
   return (
-    <>
-      <h1>Guess The Song</h1>
-    </>
+    <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Guess The Song
+      </h1>
+    </div>
   );
 }
 
